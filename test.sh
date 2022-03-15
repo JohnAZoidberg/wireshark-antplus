2757c58e4733e141e4e068100cc5f97228370db6
Daniel Schaefer <git@danielschaefer.me>
2022-03-15 21:14:49 +0800

    Decode some more messages

--- test.sh
+++ test.sh
@@ -8,3 +8,7 @@ tshark -r hrm2.pcapng -Y 'usb.src == "1.6.1" and ant.msg_id == 0x6f' -O antplus 
 # Properly parses ANT Version string
 tshark -r hrm2.pcapng -Y 'usb.src == "1.6.1" and ant.msg_id == 0x3e' -O antplus \
   | grep "ANT Version: AP2USB1.05$"
+
+# Properly decodes and calculates messaging period
+tshark -r hrm2.pcapng -Y 'usb.dst == "1.6.1" and ant.msg_id == 0x43' -O antplus \
+  | grep "Messaging Period: 8070 (4.060 Hz)$"
