5902562ecbd7705d1d4414d587f5b7547b95cbd3
Daniel Schaefer <git@danielschaefer.me>
2022-03-15 20:41:33 +0800

    test.sh: Fix pcap filename

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 # Can parse Startup Notification Message
-tshark -r hrm.pcapng -Y 'usb.src == "1.6.1" and ant.msg_id == 0x6f' -O antplus \
+tshark -r hrm2.pcapng -Y 'usb.src == "1.6.1" and ant.msg_id == 0x6f' -O antplus \
   | grep StartupNotificationMessage
 
 # Properly parses ANT Version string
-tshark -r hrm.pcapng -Y 'usb.src == "1.6.1" and ant.msg_id == 0x3e' -O antplus \
+tshark -r hrm2.pcapng -Y 'usb.src == "1.6.1" and ant.msg_id == 0x3e' -O antplus \
   | grep "ANT Version: AP2USB1.05$"
